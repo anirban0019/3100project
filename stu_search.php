7bb19cc46206451b6a22a80252c3a0c3404f9d0f
AfifaTasneemQuanita <116279495+AfifaTasneemQuanita@users.noreply.github.com>
2023-08-06 22:57:31 +0600

    some changes in searchbar and navbar

--- stu_search.php
+++ stu_search.php
@@ -2,23 +2,23 @@
 <html>
 <head>
     <title>Student Search</title>
-    <link rel="stylesheet" type="text/css" href="css/Searchbar.CSS">   
+    <link rel="stylesheet" type="text/css" href="css/Searchbar.css">   
     <link rel="stylesheet" type="text/css" href="css/style2.css">
     <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
 <body>
-    <!-- Starting session -->
+     <!-- Starting session -->
     <?php include_once "include/start_session.php" ?>
-    <?php include_once "include/navbar.php" ?>
-
+    <?php include_once "include/navbar.php" ?> 
+<div class="container">
     <h2>Search</h2>
     <form method="post" class="Search-bar">
-        <input type="radio" name="search_type" value="students" checked> Search Students
+        <input type="radio" class="student" name="search_type" value="students" checked> Search Students
         <br>
-        <input type="radio" name="search_type" value="teachers"> Search Teachers
+        <input type="radio"class="teacher" name="search_type" value="teachers"> Search Teachers
         <br><br>
-        <input type="text" name="search" placeholder="Enter your search query">
-        <input type="submit" name="submit" value="Search">
+        <input type="text" class="text" name="search" placeholder="Enter your search query">
+        <input type="submit" class="text" name="submit" value="Search">
     </form>
     <br>
 
@@ -30,5 +30,6 @@
     <form method="get" action="insert.php">
         <input type="submit" value="Insert" name="insert">
     </form>
+</div>
 </body>
 </html>
